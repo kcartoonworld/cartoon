3ca6c398c49d167d7b61bce91547cb7a19d81889
kmusw <dudals2467@kookmin.ac.kr>
2019-11-12 15:57:01 +0900

    commit

---
+++ cartoon/cartoon/app/src/main/java/com/cookandroid/cartoon/join.java
@@ -0,0 +1,38 @@
+package com.cookandroid.cartoon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class join extends MainActivity {
+    Button back, cancle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join);
+        setTitle("회원 가입 구성");
+
+        back = (Button) findViewById(R.id.bt_join_ok);
+        cancle = (Button) findViewById(R.id.bt_cancle);
+
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Toast.makeText(join.this,"회원가입 성공",Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+        cancle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                Toast.makeText(join.this,"회원가입 취소",Toast.LENGTH_SHORT).show();
+                startActivity(intent2);
+            }
+        });
+    }
+}
---
+++ cartoon/cartoon/app/src/main/java/com/cookandroid/cartoon/main.java
@@ -0,0 +1,11 @@
+package com.cookandroid.cartoon;
+
+import android.os.Bundle;
+
+public class main extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
